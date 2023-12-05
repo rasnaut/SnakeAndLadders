fca991d99a9df60c729ddbd50c94a6baa5ea5947
rasnaut <rasnaut@gmail.com>
2023-12-05 18:27:29 +0100

    Lesson 1

---
+++ Scripts/PlayerChip.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerChip : MonoBehaviour
+{
+  // Этот метод устанавливает спрайт фишки
+  public void SetSprite(Sprite sprite)
+  {
+    SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>(); // Получаем компонент SpriteRenderer, который отображает спрайты на игровой сцене
+    spriteRenderer.sprite = sprite;                                 // Устанавливаем спрайт, переданный в метод, в компонент SpriteRenderer
+  }
+}
---
+++ Scripts/PlayerChipsCreator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerChipsCreator : MonoBehaviour
+{
+  public int PlayersCount = 2;                       // Количество игроков в игре
+  public PlayerChip PlayerChipPrefab;                // Префаб фишки игрока
+  public Sprite[] PlayerChipSprites = new Sprite[0]; // Массив спрайтов фишек игроков размером 0
+
+  private void Start()
+  {
+    SpawnPlayersChips(PlayersCount);  // При запуске игры создаём фишки игроков
+  }
+
+  private void SpawnPlayersChips(int count)
+  {
+    // Перебираем количество игроков в цикле
+    for (int i = 0; i < count && i <= PlayerChipSprites.Length; i++)
+    {
+      SpawnPlayerChip(PlayerChipSprites[i]); // Создаём фишку игрока с переданным спрайтом
+    }
+  }
+
+  private void SpawnPlayerChip(Sprite sprite)
+  {
+    // Если спрайт отсутствует
+    if (!sprite) 
+      return;
+    
+    PlayerChip newPlayerChip = Instantiate(PlayerChipPrefab, transform.position, transform.rotation); // Создаём новую фишку игрока из префаба фишки
+
+    newPlayerChip.SetSprite(sprite);  // Устанавливаем спрайт фишки
+  }
+}
